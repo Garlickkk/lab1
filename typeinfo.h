4753b8c0eca6e9565d6d378e9d2d6bf4791ead92
Sergei <sl1sher@mail.ru>
2026-03-15 19:02:37 +0300

    Update typeinfo.h

--- typeinfo.h
+++ typeinfo.h
@@ -6,9 +6,11 @@
 typedef struct FieldInfo
 {
     size_t size;
-
-    void (*add)(void* a, void* b, void* result);
-    void (*mul)(void* a, void* b, void* result);
+    void (*add)(const void* a, const void* b, void* result);
+    void (*mul)(const void* a, const void* b, void* result);
+    void (*print)(const void* a);
+    int (*scan)(void* a);
+    void (*random)(void* a);
 } FieldInfo;
 
 FieldInfo* GetIntFieldInfo();
